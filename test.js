2b4d153f53c49ed8e4ba4935b081ed4c68a54573
GDL <675491258@qq.com>
2017-12-25 09:37:28 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name           去除云管家
+// @author         ted423
+// @description    # download-windows-iso;在微软官网下载windows iso镜像
+// @namespace	https://greasyfork.org/users/85
+// @license        GPL version 3
+// @include        https://www.microsoft.com/*/software-download/windows*
+// @run-at         document-start
+// @version 17.12.25
+// ==/UserScript==
+(function() {
+	navigator.__defineGetter__('platform', function() {
+		return 'Linux x86';
+	});
+	navigator.__defineGetter__('userAgent', function() {
+		return 'Mozilla/5.0 (X11; Fedora; Linux x86_64;) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.3';
+	});
+})();
